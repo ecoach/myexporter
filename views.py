a536d41983c49d1b4e02f1595e27f6dc8cb06c21
jaredtritz <jaredtritz@gmail.com>
2013-09-19 09:56:35 -0400

    updates to export for common only join on enrolled

--- views.py
+++ views.py
@@ -120,7 +120,10 @@ def download_trigger_view(request):
             cursor = connections[db].cursor()
             col_str = '`, `'.join([str(x) for x in cols]) 
             students = Source1.objects.all().order_by('id').values_list('user_id')
-            where_str = " where user_id='" + "' or user_id='".join([str(x[0]) for x in students]) + "'"
+            if str(download.table) == 'Common1':
+                where_str = " where user_id='" + "' or user_id='".join([str(x[0]) for x in students]) + "'"
+            else:
+                where_str = ""
             query = "select `" + col_str + "` from " + eval(download.table)._meta.db_table + where_str
             res = cursor.execute(query)
             done = cursor.fetchall() 
